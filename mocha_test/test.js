88b7a327435676d0ceba55d596d5680302405ccb
Jerry Zou <jerry.zry@outlook.com>
2016-01-12 00:00:01 +0800

    add test for sub/unsub

--- mocha_test/test.js
+++ mocha_test/test.js
@@ -8,7 +8,55 @@ var chai = require('chai')
   , Data = require('../data.js')
   , D = Data;
 
+/***
+ * 测试用例 tests
+ * 注意：
+ * - 在测试`sub`接口时，会监听`get`属性所定义的key
+ * - `events`指的是这一个测试用例中，会被触发的事件名序列
+ */
+
+var tests = [
+  {
+    'set': { 'a.b.c': 1 },
+    'get': { 'a.b': { c: 1 } },
+    'events': [ 'add', 'set' ]
+  },
+  {
+    'set': { 'a.b.c': [1, 2, 3] },
+    'get': {
+      'a.b.c.1': 2,
+      'a': { b:{c:[1,2,3]} }
+    },
+    'events': [ 'add', 'add', 'set', 'set' ]
+  },
+  {
+    'set': { 'a': { b:{c:[1,2,3]} } },
+    'get': { 'a.b.c.2': 3 },
+    'events': [ 'add', 'set' ]
+  },
+  {
+    'set': { 'a.b.c.d.e.f': 1 },
+    'get': { 'a.b.c.d.e': { f: 1 } },
+    'events': [ 'add', 'set' ]
+  },
+  {
+    'set': { 'a': [ {b:1} ] },
+    'get': {
+      'a.0.b': 1,
+      'a': [ {b:1} ]
+    },
+    'events': [ 'add', 'add', 'set', 'set' ]
+  }
+];
+
+function testcase(test) {
+  return 'testcase: ' + JSON.stringify(test);
+}
+
 describe('Data.js', function() {
+  // set timeout for asynchronous code
+  this.timeout(100);
+
   describe('get & set', function() {
 
     it('simple get & set', function() {
@@ -17,14 +65,6 @@ describe('Data.js', function() {
       expect(D.get('a')).to.equal(1);
       D.clear();
     });
-
-    var tests = [
-      { 'set': { 'a.b.c': 1 }, 'get': { 'a.b': { c: 1 } } },
-      { 'set': { 'a.b.c': [1, 2, 3] }, 'get': { 'a.b.c.1': 2, 'a': { b:{c:[1,2,3]} } }},
-      { 'set': { 'a': { b:{c:[1,2,3]} }, 'get': { 'a.b.c.2': 3 } }},
-      { 'set': { 'a.b.c.d.e.f': 1 }, 'get': { 'a.b.c.d.e': { f: 1 } }},
-      { 'set': { 'a': [ {b:1} ] }, 'get': { 'a.0.b': 1, 'a': [ {b:1} ] }}
-    ];
 
     tests.forEach(function(test, index) {
       it('complex get & set - ' + index, function() {
@@ -35,7 +75,7 @@ describe('Data.js', function() {
         }
         for (key in test.get) {
           if (test.get.hasOwnProperty(key)) {
-            expect(D.get(key)).to.deep.equal(test.get[key]);
+            expect(D.get(key), testcase(test)).to.deep.equal(test.get[key]);
           }
         }
         D.clear();
@@ -52,9 +92,10 @@ describe('Data.js', function() {
   });
 
   describe('sub', function() {
+
     it('simple sub', function(done) {
       var target
-        , events = ['add', 'set', 'set', 'update']
+        , events = ['add', 'delete', 'set', 'set', 'set', 'update']
         , eventStack = []
         , tryDone = function() {
           if (eventStack.length >= events.length) {
@@ -65,26 +106,83 @@ describe('Data.js', function() {
           }
         };
 
-      D.sub('set', 'a', function (e) {
-        expect(e).to.deep.equal({ type: 'set', key: 'a', data: target });
-        eventStack.push('set');
-        tryDone();
-      });
-      D.sub('add', 'a', function (e) {
-        expect(e).to.deep.equal({ type: 'add', key: 'a', data: target });
-        eventStack.push('add');
-        tryDone();
-      });
-      D.sub('update', 'a', function (e) {
-        expect(e).to.deep.equal({ type: 'update', key: 'a', data: target });
-        eventStack.push('update');
-        tryDone();
+      ['set', 'add', 'delete', 'update'].forEach(function(eventName) {
+        D.sub(eventName, 'a', function (e) {
+          expect(e).to.deep.equal({ type: eventName, key: 'a', data: target });
+          eventStack.push(eventName);
+          tryDone();
+        });
       });
 
       target = 1;
       D.set('a', 1);
       target = 2;
       D.set('a', 2);
+      target = null;
+      D.set('a', null);
+    });
+
+    tests.forEach(function(test, index) {
+      it('complex sub - ' + index, function(done) {
+        var events = test.events
+          , eventStack = []
+          , tryDone = function() {
+            if (eventStack.length >= events.length) {
+              eventStack.sort(function(a, b) { return a > b; });
+              events.sort(function(a, b) { return a > b; });
+              expect(eventStack, testcase(test)).to.deep.equal(events);
+              D.clear();
+              done();
+            }
+          };
+
+        for (key in test.get) {
+          if (test.get.hasOwnProperty(key)) {
+            (function(key) {
+              ['set', 'update', 'delete', 'add'].forEach(function(eventName) {
+                D.sub(eventName, key, function (e) {
+                  expect(e).to.deep.equal({ type: eventName, key: key, data: test.get[key] });
+                  eventStack.push(eventName);
+                  tryDone();
+                });
+              });
+            }(key));
+          }
+        }
+
+        for (var key in test.set) {
+          if (test.set.hasOwnProperty(key)) {
+            D.set(key, test.set[key]);
+          }
+        }
+      });
+    });
+  });
+
+  describe('unsub', function() {
+    it('simple test', function(){
+      var triggerTimes = 0
+        , eid = D.sub('set', 'm', function (e) { triggerTimes++; });
+      D.set('m', 1);
+      D.set('m', 2);
+      D.unsub('set', 'm', eid);
+      D.set('m', 3);
+      D.set('m', 4);
+      D.clear();
+      expect(triggerTimes).to.equal(2);
+    });
+  });
+
+  describe('multiple Data', function() {
+    it('simple test', function() {
+      var A = new Data();
+      var B = new Data();
+
+      A.set('a', 123);
+      B.set('a', 456);
+
+      expect(A.get('a')).to.equal(123);
+      expect(B.get('a')).to.equal(456);
     });
   });
 });
